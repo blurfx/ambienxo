8095c0cc88f6bc5e71f1da915125ab2b0c770f6e
blurfx <iam@xo.dev>
2023-03-27 04:55:23 +0900

    feat: add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,8 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <link rel={'icon'} sizes='32x32' href={'/favicon32.png'} />
+        <link rel={'icon'} sizes='96x96' href={'/favicon96.png'} />
         <style dangerouslySetInnerHTML={{ __html: getCssText() }} />
       </Head>
       <body>
